d16c7d983b9c9014889564edaf25c939cb68ca54
William-Rojas117 <haroparts117@gmail.com>
2025-05-28 14:19:29 -0500

    Update dim_dias_semana_insert.sql

--- DataWahrehouse/Scripst/dim_dias_semana_insert.sql
+++ DataWahrehouse/Scripst/dim_dias_semana_insert.sql
@@ -1,10 +1,10 @@
--- Crear tabla de productos con campos
--- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
-
-
-CREATE TABLE DataWarehouse.dbo.dim_Dias_Semana (
-	DiaSemana tinyint IDENTITY(1,1) NOT NULL,
-	NomDiaSemana nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	NomCortoDiaSemana char(3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	CONSTRAINT DIM_Dias_Semana_PK PRIMARY KEY (DiaSemana)
-);
+INSERT INTO DataWareHouse.dbo.DIM_Dias_Semana
+(NomDiaSemana, NomCortoDiaSemana)
+VALUES
+('Lunes', 'LUN'),
+('Martes', 'MAR'),
+('Miércoles', 'MIE'),
+('Jueves', 'JUE'),
+('Viernes', 'VIE'),
+('Sábado', 'SAB'),
+('Domingo', 'DOM')
