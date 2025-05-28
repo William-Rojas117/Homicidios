3ee3ca86a8842b0895e82fbf719c6afb4c06c922
William-Rojas117 <haroparts117@gmail.com>
2025-05-28 14:25:04 -0500

    Create dim_zonas_create.sql

---
+++ DataWahrehouse/Scripst/dim_zonas_create.sql
@@ -0,0 +1,8 @@
+-- Crear tabla de productos con campos
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
+CREATE TABLE DataWarehouse.dbo.dm_zonas (
+	id_zona tinyint IDENTITY(1,1) NOT NULL,
+	nom_zona nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	CONSTRAINT dm_zonas_pk PRIMARY KEY (id_zona)
+);
