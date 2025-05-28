9ee8e66396fddcf0f627713134fff7b62924dc87
William-Rojas117 <haroparts117@gmail.com>
2025-05-28 13:55:25 -0500

    Create dl_homicidios-create.sql

---
+++ DataLake/Scrips/dl_homicidios-create.sql
@@ -0,0 +1,16 @@
+-- Crear tabla de productos con campos
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
+
+IF OBJECT_ID('dbo.DL_Homicidios', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DL_Homicidios (
+		FECHA_HECHO date NULL,
+		COD_DEPTO tinyint NULL,
+		DEPARTAMENTO nvarchar(60) NULL,
+		COD_MUNI int NULL,
+		MUNICIPIO nvarchar(30) NULL,
+		ZONA nvarchar(10) NULL,
+		SEXO nvarchar(20) NULL,
+		CANTIDAD tinyint NULL
+	);
